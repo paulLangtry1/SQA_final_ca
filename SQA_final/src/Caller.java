e0869eaf4c7e6971b1bffe7be1f94202b18f72cf
Paul Langtry <paullangtry2010@gmail.com>
2021-05-19 20:46:41 +0100

    Early commit to display Test driven development approach

--- SQA_final/src/Caller.java
+++ SQA_final/src/Caller.java
@@ -1,6 +1,39 @@
+import java.util.ArrayList;
 
 public class Caller 
 {
 
 	//this acts as the driver class to run my test
+	
+	ArrayList<Rubric> rubrics = new ArrayList<Rubric>();
+	
+	public Caller()
+	{
+		
+	}
+	
+	public Rubric createRubric(ArrayList<String> criteria, String title)
+	{
+		Rubric rubric = new Rubric(title);
+		rubric.setCriteria(criteria);
+		rubrics.add(rubric);
+		
+		return rubric;
+	}
+	
+	public ArrayList<Rubric> getAllRubrics() 
+	{
+		return rubrics;
+	}
+
+	public Rubric getSpecificRubric(String title) 
+	{
+
+		for (Rubric rubric : rubrics) {
+			if (rubric.getTitle().equalsIgnoreCase(title)) {
+				return rubric;
+			}
+		}
+		return null;
+	}
 }
--- SQA_final/src/UnitTests.java
+++ SQA_final/src/UnitTests.java
@@ -1,7 +1,34 @@
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 public class UnitTests 
 {
 	
 	//this is where my unittests will be performed
+	
+	Caller caller;
+	
+	
+		@Test
+		public void createRubrictest() 
+		{
+
+			 caller = new Caller();
+
+			ArrayList<String> criteria = new ArrayList<>();
+			criteria.add(new String("SQA"));
+			criteria.add(new String("OOSD"));
+			criteria.add(new String("Accounting"));
+
+			Rubric rubric = caller.createRubric(criteria, "modules");//creating rubric for testing
+			
+			Assert.assertFalse(caller.getAllRubrics().isEmpty());
+			Assert.assertTrue(rubric.getCriteria().equals(criteria));
+			Assert.assertTrue(rubric.getTitle().equals("modules"));
+
+		}
+
 
 }
